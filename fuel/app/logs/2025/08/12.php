81e82275a36c068416805945b0b1daa8d9b899a9
Mmm-uu <mayuonishi0521@gmail.com>
2025-08-18 10:50:27 +0900

    最初のコミット

---
+++ fuel/app/logs/2025/08/12.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-08-12 09:37:57 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-08-12 09:57:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
